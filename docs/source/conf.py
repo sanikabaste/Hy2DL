24f5ad42c5eb4ba2c3544862ee8f584c9d487a72
sanikabaste <161843404+sanikabaste@users.noreply.github.com>
2024-08-21 19:17:56 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,19 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import datetime
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../../'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -43,7 +56,8 @@ autodoc_mock_imports = [
     'xarray',
 ]
 
-
+# -- Napoleon autodoc options -------------------------------------------------
+napoleon_numpy_docstring = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -51,3 +65,14 @@ autodoc_mock_imports = [
 html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
+
+# Allows to build the docs with a minimal environment without warnings about missing packages
+autodoc_mock_imports = [
+    'matplotlib',
+    'numba',
+    'pandas',
+    'ruamel',
+    'scipy',
+    'tqdm',
+    'xarray',
+]
