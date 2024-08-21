398567c61832b926b4aa24328ff6b6eecdd2982d
sanikabaste <161843404+sanikabaste@users.noreply.github.com>
2024-08-21 14:29:50 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,15 +15,31 @@ release = '1.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',  # autodocument
+    'sphinx.ext.napoleon',  # google and numpy doc string support
+    'sphinx.ext.mathjax',  # latex rendering of equations using MathJax
+    'nbsphinx',  # for direct embedding of jupyter notebooks into sphinx docs
+    'nbsphinx_link'  # to be able to include notebooks from outside of the docs folder
 ]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['**.ipynb_checkpoints', '_build']
 
 templates_path = ['_templates']
 exclude_patterns = []
+
+# Allows to build the docs with a minimal environment without warnings about missing packages
+autodoc_mock_imports = [
+    'matplotlib',
+    'numba',
+    'pandas',
+    'ruamel',
+    'scipy',
+    'tqdm',
+    'xarray',
+]
 
 
 
