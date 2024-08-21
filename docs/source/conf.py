d688a40f75c7e7e92d066595f5dbc4473f991988
sanikabaste <161843404+sanikabaste@users.noreply.github.com>
2024-08-21 13:36:49 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,13 @@ release = '1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +30,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
 html_static_path = ['_static']
