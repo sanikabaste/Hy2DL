5723ec1813d1378aa436c0bd7c67e14bf618528c
sanikabaste <161843404+sanikabaste@users.noreply.github.com>
2024-08-22 12:59:52 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.append('../../Hy2DL')
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
