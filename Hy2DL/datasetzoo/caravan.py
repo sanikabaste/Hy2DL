bc0919f92a0a4c49b592df72e840758102b47b7a
sanikabaste <161843404+sanikabaste@users.noreply.github.com>
2024-08-22 13:44:09 +0200

    Update caravan.py

--- Hy2DL/datasetzoo/caravan.py
+++ Hy2DL/datasetzoo/caravan.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 from typing import List, Optional
-from basedataset import BaseDataset
+from Hy2DL.datasetzoo.basedataset import BaseDataset
 
 
 
